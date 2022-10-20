aa06b1f3fead8fdf5f032f8717775f87e7618a7c
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-20 19:50:25 +0200

    UseState is started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Icon from "./components/Icon";
+import "./App.css";
+
+// React Toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+//React Boostrap
+import { Card, CardBody, Container, Button, Col, Row  } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const itemArray = new Array(9).fill("empty")
 
 const App =()=>{
+
+    const [isCross, setIsCross] = useState(false);
+    const [winMessage, setWinMessage] = useState("");
+
+    const reloadGame = () =>{
+        //
+    }
+    const checkWinner = () =>{
+        //
+    }
+    const changeItem = itemNumber => {
+        //
+    }
     return(
         <div>
             <Icon />
--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,16 +2,17 @@ import React from "react";
 import { FaTimes, FaPen, FaRegCircle } from 'react-icons/fa';
 
 
-const Icon = ({names}) => {
-    return (
-        <div>
-            <h1>Hello</h1>
-            <FaTimes  className="icon"/>
-            <FaPen className="icon"/>
-            <FaRegCircle className="icon"/>
 
-        </div>
-    )
-}
+const Icon = ({names}) => {
+            switch (names) {
+                case "circle":
+                   return <FaRegCircle className="icon"/>
+                     case "Times":
+                   return <FaTimes className="icon"/>
+                     default:
+                   return   <FaPen className="icon"/>
+               
+            }
+        }
 
 export default Icon;
